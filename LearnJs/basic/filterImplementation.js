76963052c8c13066f03190e31add59a069224963
Vivekanand-s-naik <vivekanadsnaik.iet@srinivasuniversity.edu.in>
2026-08-11 21:57:55 +0530

    -

--- LearnJs/basic/filterImplementation.js
+++ LearnJs/basic/filterImplementation.js
@@ -1,18 +1,44 @@
-let arr = [0,10,20,30];
+// let arr = [0,10,20,30];
 
-function applyFilter(arr, fn){
-    let 
-}
-
-// function gtThen10(num){
-//     return num  > 10;
+// function applyFilter(arr = [], fn){
+//     let resArr = [];
+//     for (let elem of arr){
+//         if (fn(elem)){
+//             resArr.push(elem);
+//         }
+//     }
+//     return resArr;
 // }
 
-// let res1 = arr. filter(gtThen10);
-// console.log(res1);
+// // function gtThen10(num){
+// //     return num  > 10;
+// // }
+
+// // let res1 = arr. filter(gtThen10);
+// // console.log(res1);
 
 
-let res = applyFilter(arr, (num) => (num >10));
+// let res = applyFilter(arr, (num) => (num >10));
 
 
-console.log(res);
+// console.log(res);
+
+let arr = [1,2,3];
+
+function firstIndex(n, i) { 
+    return i === 0; 
+}
+
+var filter = function(arr, fn, ...args) {
+    console.log(args);
+    
+    let resArr = [];
+    for(let elem of arr){
+        if (fn(elem, args)) resArr.push(elem);
+    }
+    return resArr;
+};
+
+console.log(filter(arr, firstIndex));
+
+
