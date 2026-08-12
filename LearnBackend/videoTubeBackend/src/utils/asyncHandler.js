467e7974f04a559ba65fc3f0645f6f430de5a684
Vivekanand-s-naik <vivekanadsnaik.iet@srinivasuniversity.edu.in>
2026-08-13 00:18:39 +0530

    Custom API Error handling

--- LearnBackend/videoTubeBackend/src/utils/asyncHandler.js
+++ LearnBackend/videoTubeBackend/src/utils/asyncHandler.js
@@ -1,3 +1,23 @@
+const asyncHandler = (func) =>(req, res, next)=>{
+    Promise.resolve(func(req, res)).catch((err)=>{
+        next(err);
+        res.status(err.code || 500).json({
+            status: false,
+            message: err.message
+        })
+    })
+}
+
+export {asyncHandler};
+
+
+
+
+
+
+
+
+
 // const asyncHandler = (func) =>{
 //     return async (req, res, next) =>{
 //         try{
@@ -11,15 +31,3 @@
 //     }
 // }
 
-
-const asyncHandler = (func) =>(req, res, next)=>{
-    Promise.resolve(func(req, res)).catch((err)=>{
-        next(err);
-        res.status(err.code || 500).json({
-            status: false,
-            message: err.message
-        })
-    })
-}
-
-export {asyncHandler};
