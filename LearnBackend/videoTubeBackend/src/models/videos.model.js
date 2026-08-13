e3d7f3330880664279acf6054a25ffb91e5e1dd4
Vivekanand-s-naik <vivekanadsnaik.iet@srinivasuniversity.edu.in>
2026-08-13 18:13:24 +0530

    Added Video  Data Model

---
+++ LearnBackend/videoTubeBackend/src/models/videos.model.js
@@ -0,0 +1,45 @@
+import mongoose, { Schema } from "mongoose";
+import { User } from "./user.model";
+
+const videoSchema = new Schema({
+    videoFile = {
+        type: String,
+        required: true
+    },
+
+    thumbnail= {
+        type: String,
+        required: true
+    },
+
+    owner = {
+        type: Schema.Types.ObjectId;
+        reference: "User"
+    },
+
+    title= {
+        type: String,
+        required: true
+    },
+
+    description= {
+        type: String,
+    },
+
+    duration = {
+        type: Number,
+        required: true
+    },
+
+    views = {
+        type: Number,
+        required: true
+    },
+
+    isPublished = {
+        type: Boolean,
+        default: false
+    }
+}, {timestamps: true});
+
+export const Video = mongoose.model("Video", videoSchema);
