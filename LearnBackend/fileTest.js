1f0f565bba7976f76ce58e8f09b4dc7c32362e6e
Vivekanand-s-naik <vivekanadsnaik.iet@srinivasuniversity.edu.in>
2026-08-15 23:39:41 +0530

    File Handling

--- LearnBackend/fileTest.js
+++ LearnBackend/fileTest.js
@@ -1,4 +1,8 @@
-import fs from "fs";
-const data = fs.readFileSync("data.txt", "utf-8");
-console.log(data);
+// import fs from "fs";
+// const data = fs.readFileSync("data.txt", "utf-8");
+// console.log(data);
+
+
+const data = "cloud_name=\napi_key=\napi_secret=".toUpperCase();
+console.log(data)
 
--- LearnBackend/videoTubeBackend/src/utils/fileUploads.js
+++ LearnBackend/videoTubeBackend/src/utils/fileUploads.js
@@ -1,6 +1,6 @@
 import { v2 as cloudinary } from "cloudinary";
 import multer from "multer";
-
+import fs from "node:fs/promises";
 
 /*
     File Storing Mechanisms
@@ -9,8 +9,32 @@ import multer from "multer";
     3. upload the file to cloudinary and delete it. 
 */
 
-cloudinary.config({ 
-  cloud_name: 'my_cloud_name', 
-  api_key: 'my_key', 
-  api_secret: 'my_secret'
+cloudinary.config({
+    cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+    api_key: process.env.CLOUDINARY_API_KEY,
+    api_secret: process.env.CLOUDINARY_API_SECRET
 });
+
+export const uploadToCloudinary = async (fileLocalPath) => {
+    try {
+        if (!fileLocalPath) return null;
+        const res = await cloudinary.uploader.upload(fileLocalPath, {
+            resource_type: "auto",
+            overwrite: true,
+            // notification_url: "https://mysite.example.com/notify_endpoint"
+        });
+        console.log("File Saved Successfully\n", res);
+        return res;
+    }catch(error){
+        console.log("Error Uploading File...\n", error.message);
+        
+        return null;
+    }finally{
+        try {
+            await fs.unlink(fileLocalPath);
+        } catch (error) {
+            console.log("Skipping the file deletion...\n(filepath may be invalid or file is already Deleted)");
+        }
+    }
+}
+
